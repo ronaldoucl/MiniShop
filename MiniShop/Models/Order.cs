05255ed05a89ffbbb509ba4a915e80ebd12b6edd
ronaldoucl <ronaldocl9lda@gmail.com>
2026-02-18 23:34:59 -0600

    products, cart and checkout

--- MiniShop/Models/Order.cs
+++ MiniShop/Models/Order.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public DateTime OrderDate { get; set; }
         public decimal Total { get; set; }
+        public string Status { get; set; } = "Pending";
 
         public int UserId { get; set; }
         public User User { get; set; } = null!;
--- MiniShop/Program.cs
+++ MiniShop/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
 using MiniShop.Components;
 using MiniShop.Data;
@@ -15,6 +16,8 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<CartService>();
+builder.Services.AddScoped<ProtectedSessionStorage>();
 
 var app = builder.Build();
 
